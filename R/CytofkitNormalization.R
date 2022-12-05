b09f3d40e51927ddca5d6ff7f6b6694b2cf9bf6c
paul-shannon <paul.thurmond.shannon@gmail.com>
2022-12-05 12:43:47 -0800

    1st working version, normalized via regression against H3 tested

--- R/CytofkitNormalization.R
+++ R/CytofkitNormalization.R
@@ -14,7 +14,8 @@ CytofkitNormalization = R6Class("CytofkitNormalization",
     private = list(filename=NULL,
                    mtx=matrix(),
                    tbl.tsne=data.frame(),
-                   clusters=list()
+                   clusters=list(),
+                   markers=list()
                    ),
 
     #--------------------------------------------------------------------------------
@@ -49,7 +50,59 @@ CytofkitNormalization = R6Class("CytofkitNormalization",
         #' @return the matrix
         getClusters = function(){
             invisible(private$clusters)
-            }
+        },
+        #------------------------------------------------------------
+        #' @description accessor for markers used in the cytof
+        #' @return nothing
+        createSimpleMarkerNames = function(){
+            candidates <- colnames(private$mtx)
+
+            for(notUseful in c("barcode", "center", "offset", "width", "residual", "environ",
+                               "dna", "live-dead", "bckg", "bi209di", "beads")){
+               deleters <- grep(notUseful, candidates, ignore.case=TRUE)
+               if(length(deleters) > 0)
+                   candidates <- candidates[-deleters]
+               }
+            markers <- candidates
+            tokens <- strsplit(markers, "_")
+            tokens.2 <- lapply(tokens, "[", 2)
+            short.names <- sub(">", "", tokens.2)
+            names(markers) <- short.names
+            private$markers <- markers
+            }, # getMarkers
+
+        #------------------------------------------------------------
+        #' @description accessor for named list, e.g. list("H3"="Yb176Di<176Yb_H3>")
+        #' @return character list
+        getMarkers = function(){
+            private$markers
+            }, # getMarkerShortNames
+
+
+        #------------------------------------------------------------
+        #' @description calculate and append a normalized vector column for the specified
+        #'   histone marker, regressed against the base ("total") H3 and/or H4 vector
+        #' @param target.marker character, a column name from the matrix, e.g., "Sm154Di<154Sm_H3K27me2>"
+        #' @param reference.markers character, the marker names for reference ("total") H3 and/or H4
+        #' @return character the newly created column name
+        normalizeMarker = function(target.marker, reference.markers){
+            printf("--- entering normalizeMarker")
+            stopifnot(all(c(target.marker, reference.markers)%in% names(private$markers)))
+            columns.requested <- c(target.marker, reference.markers)
+            columns.actual <- as.character(lapply(columns.requested,
+                                                   function(shortName) private$markers[[shortName]]))
+
+            mtx.2 <- private$mtx[, columns.actual]
+            colnames(mtx.2) <- columns.requested
+            model <- sprintf("%s ~ 1 + %s", target.marker, paste(reference.markers, collapse=" + "))
+            fit <- lm(model, data=as.data.frame(mtx.2))
+            marker.resid <- as.numeric(residuals(fit))
+            new.col.name <- sprintf("%s.regress.%s", target.marker, paste(paste(reference.markers, collapse="+")))
+            private$mtx <- cbind(private$mtx, new.col.name=marker.resid)
+            colnames(private$mtx)[ncol(private$mtx)] <- new.col.name
+            new.col.name
+            } # normalizeMarker
+
        ) # public
 
     ) # class
--- inst/unitTests/test_CytofkitNormalization.R
+++ inst/unitTests/test_CytofkitNormalization.R
@@ -4,6 +4,8 @@ library(CytofkitNormalization)
 runTests <- function()
 {
     test_ctor()
+    test_markerNames()
+    test_normalizeMarker()
 
 } # runTests
 #----------------------------------------------------------------------------------------------------
@@ -27,6 +29,73 @@ test_ctor <- function()
     checkEquals(clusters[["CBday4_2022-09-16_ery-MLL_Histone_LIVE_46719"]], 1)
 
 } # test_ctor
+#----------------------------------------------------------------------------------------------------
+test_markerNames <- function()
+{
+    message(sprintf("--- test_markerNames"))
+
+    f <- system.file(package="CytofkitNormalization", "extdata", "cytofkit-leukemia.RData")
+    checkTrue(file.exists(f))
+    x <- CytofkitNormalization$new(f)
+
+    x$createSimpleMarkerNames()
+
+    markers <- x$getMarkers()
+    h3.markers <- grep("H3", names(markers), v=TRUE)
+    checkEquals(length(h3.markers), 20)
+    h4.markers <- grep("H4", names(markers), value=TRUE)
+    checkEquals(length(h4.markers), 4)
+
+    checkTrue("H3" %in% h3.markers)
+    checkTrue("H4" %in% h4.markers)
+
+    checkEquals(markers[["H3"]], "Yb176Di<176Yb_H3>")
+    checkEquals(markers[["H4"]], "Nd142Di<142Nd_H4>")
+
+    checkTrue(length(markers) > 35)
+    checkTrue(length(markers) < 45)
+
+} # test_markerNames
+#----------------------------------------------------------------------------------------------------
+test_normalizeMarker <- function()
+{
+    message(sprintf("--- test_normalizeMarker"))
+
+    f <- system.file(package="CytofkitNormalization", "extdata", "cytofkit-leukemia.RData")
+    checkTrue(file.exists(f))
+    x <- CytofkitNormalization$new(f)
+
+    x$createSimpleMarkerNames()
+    markers <- x$getMarkers()
+
+    h3.markers <- grep("H3", markers, value=TRUE)
+    h4.markers <- grep("H4", markers, value=TRUE)
+
+    target <- markers[["H3K4me3"]]
+    h3.reference <- markers[["H3"]]
+    h4.reference <- markers[["H4"]]
+
+    target <- "H3K4me3"
+    h3.reference <- "H3"
+    h4.reference <- "H4"
+
+    new.col.name <- x$normalizeMarker(target, h3.reference)
+    checkEquals(new.col.name, "H3K4me3.regress.H3")
+
+      # a hasty un-nuanced check is to get the quartiles of
+      # of the distribution before and after normaliaztion,
+      # check their sums
+
+    normalized.vec <- x$getMatrix()[, new.col.name]
+    quick.assay.normalized <- sum(as.numeric(fivenum(normalized.vec)))
+       # -4.32963408 -0.25168910  0.01600153  0.29289136  1.89312134
+    checkTrue(quick.assay.normalized < -2)
+    raw.vec <- x$getMatrix()[, markers[["H3K4me3"]]]
+    quick.assay.raw <- sum(as.numeric(fivenum(raw.vec)))
+       #  -0.006413926  3.504356736  3.804160211  4.115765684  5.413913164
+    checkTrue(quick.assay.raw > 16)
+
+} # test_normalizeMarker
 #----------------------------------------------------------------------------------------------------
 if(!interactive())
     runTests()
